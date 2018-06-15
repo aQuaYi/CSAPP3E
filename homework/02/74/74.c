86b9085ab5d6036d08342e2b94ae7ae4fa4e2052
aQua <aQua@aQuaYi.com>
2018-06-15 09:07:37 +0800

    2.74 added

--- homework/02/74/74.c
+++ homework/02/74/74.c
@@ -7,11 +7,17 @@
 
 int tsub_ok(int x, int y)
 {
+    int res = 1;
+
+    (y == INT_MIN) && (res = 0);
+
     int sub = x - y;
-    int sig_mask = INT_MIN;
-    /**
-     * 
-     */
+    int pos_over = x > 0 && y < 0 && sub < 0;
+    int neg_over = x < 0 && y > 0 && sub > 0;
+
+    res = res && !(pos_over || neg_over);
+
+    return res;
 }
 
 int main()
@@ -30,6 +36,12 @@ int main()
 
     y = -y;
     printf("tsub_ok(0x%08x, 0x%08x) = %d\n", x, y, tsub_ok(x, y));
+
+    x = 0;
+    printf("tsub_ok(0x%08x, 0x%08x) = %d\n", x, INT_MIN, tsub_ok(x, INT_MIN));
+
+    x = 1;
+    printf("tsub_ok(0x%08x, 0x%08x) = %d\n", x, INT_MIN, tsub_ok(x, INT_MIN));
 
     return 0;
 }
