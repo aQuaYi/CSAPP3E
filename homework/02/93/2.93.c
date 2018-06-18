6c95835e1dffe50659061972a2e0576704e17854
aQua <aQua@aQuaYi.com>
2018-06-18 18:11:46 +0800

    2.93 added

--- homework/02/93/2.93.c
+++ homework/02/93/2.93.c
@@ -5,12 +5,15 @@ typedef unsigned float_bits;
 // Compute |f|. If f is NaN, then return f.
 float_bits float_absval(float_bits f)
 {
-    unsigned sign = f >> 31;
+    unsigned sig = f >> 31;
+    unsigned exp = f >> 23 & 0xFF;
+    unsigned frac = f & 0x7FFFFF;
 
-    if (sign == 1)
+    int is_NAN = (exp == 0xFF) && (frac != 0);
+    if (is_NAN)
     {
-        return float_negate(f);
+        return f;
     }
 
-    return f;
+    return 0 << 31 | exp << 23 | frac;
 }
