371e3661c80b62dd0e10ba6c1edb76e81402a4bb
aQua <aQua@aQuaYi.com>
2018-06-19 13:43:03 +0800

    03 begin

---
+++ homework/03/execise/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void multstore(long, long, long *);
+
+int main(int argc, char const *argv[])
+{
+    long d;
+    multstore(2, 3, &d);
+    printf("2 * 3 --> %ld\n", d);
+    return 0;
+}
+
+long mult2(long a, long b)
+{
+    long s = a * b;
+    return s;
+}
