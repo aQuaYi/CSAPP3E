30ccf6d432732715bf5abfe90c81dcfdd80aec69
aQua <aQua@aQuaYi.com>
2018-07-11 15:17:37 +0800

    add something

---
+++ homework/05/05/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    double a = 1.0;
+    double b = 2.0;
+    double c = 3.0;
+    a += b * c;
+}
---
+++ homework/05/05/main.s
@@ -0,0 +1,45 @@
+	.file	"main.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movsd	.LC0(%rip), %xmm0
+	movsd	%xmm0, -24(%rbp)
+	movsd	.LC1(%rip), %xmm0
+	movsd	%xmm0, -16(%rbp)
+	movsd	.LC2(%rip), %xmm0
+	movsd	%xmm0, -8(%rbp)
+	movsd	-16(%rbp), %xmm0
+	mulsd	-8(%rbp), %xmm0
+	movsd	-24(%rbp), %xmm1
+	addsd	%xmm1, %xmm0
+	movsd	%xmm0, -24(%rbp)
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.section	.rodata
+	.align 8
+.LC0:
+	.long	0
+	.long	1072693248
+	.align 8
+.LC1:
+	.long	0
+	.long	1073741824
+	.align 8
+.LC2:
+	.long	0
+	.long	1074266112
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.10) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
