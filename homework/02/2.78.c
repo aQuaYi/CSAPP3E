3b11e96792ae0fa3258b19bf34ef4959f85becc0
aQua <aQua@aQuaYi.com>
2018-07-26 10:10:57 +0800

    02 added 78

--- homework/02/2.78.c
+++ homework/02/2.78.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-
-/**
- * Divide by power of 2. Assume 0 <= k < w-1
- */
+#include <assert.h>
+#include <limits.h>
 
 int divide_power2(int x, int k)
 {
-    int w = sizeof(int) << 3;
-    int ui = (unsigned)-1 >> (w - k);
-    int bias = (x >> (w - 1)) & ui;
-    return (x + bias) >> k;
+    int x_is_neg = x & INT_MIN;
+    x_is_neg && (x += (1 << k) - 1);
+    return x >> k;
 }
 
 int main(int argc, char const *argv[])
 {
 
     int x = 0x72345678;
+    int tx = x;
     for (int k = 0; k < 31; ++k)
     {
         printf("%d / 2^%d = %d\n", x, k, divide_power2(x, k));
+        assert(divide_power2(x, k) == tx);
+        tx /= 2;
     }
 
     x = 0x87654321;
+    tx = x;
     for (int k = 0; k < 31; ++k)
     {
         printf("%d / 2^%d = %d\n", x, k, divide_power2(x, k));
+        assert(divide_power2(x, k) == tx);
+        tx /= 2;
     }
 
     return 0;
