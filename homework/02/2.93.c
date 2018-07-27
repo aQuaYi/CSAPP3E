6f71d4d89fb58a696d22a3b403a5484caee1c7d9
aQua <aQua@aQuaYi.com>
2018-07-27 11:31:42 +0800

    02 add 93

--- homework/02/2.93.c
+++ homework/02/2.93.c
@@ -4,25 +4,22 @@
 #include <limits.h>
 #include "float-bits.h"
 
-/* Compute -f. If f is NaN, then return f. */
-float_bits float_negate(float_bits f)
+/* Compute |f|. If f is NaN, then return f. */
+float_bits float_absval(float_bits f)
 {
     /* Decompose bit representation into parts */
-    unsigned sign = f >> 31;
     unsigned exp = f >> 23 & 0xFF;
     unsigned frac = f & 0x7FFFFF;
 
-    int is_NaN = exp == 0xFF && frac != 0;
+    int is_NaN = (exp == 0xFF && frac != 0);
 
     if (is_NaN)
     {
         return f;
     }
 
-    sign ^= 1;
-
     // Ressemble bits
-    return (sign << 31) | (exp << 23) | frac;
+    return (exp << 23) | frac;
 }
 
 int main(int argc, char const *argv[])
@@ -31,8 +28,8 @@ int main(int argc, char const *argv[])
 
     for (; fb <= 0xFFFFFFFF; fb++)
     {
-        printf("float_negate(0x%.8X) = 0x%.8X\n", fb, float_negate(fb));
-        assert(fb2f(float_negate(fb)) == -fb2f(fb));
+        printf("float_absval(0x%.8X) = 0x%.8X\n", fb, float_absval(fb));
+        assert(fb2f(float_absval(fb)) == fabs(fb2f(fb)));
     }
 
     return 0;
