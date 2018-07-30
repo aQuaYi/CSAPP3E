3275445aa8a83a192e743204f20d0607d330af31
aQua <aQua@aQuaYi.com>
2018-07-30 14:46:16 +0800

    02.97 finish

--- homework/02/2.97.c
+++ homework/02/2.97.c
@@ -8,78 +8,69 @@
 */
 float_bits float_i2f(int i)
 {
+
+    // 0 是 int 中的唯一的非规格化数，单独处理
     if (i == 0)
     {
-        return 0;
+        return 0x0;
     }
 
+    // 由于 -INT_MIN = INT_MIN
+    // 后面的 M 无法正确表示，单独处理
     if (i == INT_MIN)
     {
         return 0xCF000000;
     }
 
     unsigned sign = i < 0 ? 1 : 0;
-    unsigned abs_i = i < 0 ? -i : i;
-    unsigned M = abs_i;
+    unsigned M = i < 0 ? -i : i;
     unsigned E = 0;
 
-    while (M > 1)
+    unsigned t = M;
+    while (t > 1)
     {
-        M >>= 1;
+        t >>= 1;
         E++;
     }
 
-    // printf("1.E=%d\n", E);
-    // printf("1.abs_i=%d, %.8X\n", abs_i, abs_i);
-
-    unsigned exp = E + 0x7F;
-    M = abs_i;
-
-    // printf("1.M=0x%X\n", M);
-
-    if (E <= 23)
+    if (E <= 23) // float 可以精确表示 int
     {
-        M <<= (23 - E);
-        M -= 1 << 23;
+        M <<= 23 - E;
     }
-    else // E > 23
+    else // E > 23， 需要进行取舍
     {
+        unsigned trunc_mask = (unsigned)-1 >> (32 - (E - 23));
+        unsigned truncation = trunc_mask & M;
+        // lsb = Least Significant Bit ， 最低有效位
         unsigned lsb_is_one = (M >> (E - 23)) & 1;
-        unsigned invalid_bit = M & ((1 << (E - 23)) - 1);
-        unsigned half_lsb = 1 << (E - 24);
-        unsigned round_to_even = (lsb_is_one && invalid_bit >= half_lsb) ||
-                                 (!lsb_is_one && invalid_bit > half_lsb);
-
-        // printf("1.lsb_is_one = %d, invalid_bit=%.8X\n", lsb_is_one, invalid_bit);
-        // printf("1.round_to_even=%d\n", round_to_even);
-
-        M >>= (E - 23);
+        unsigned half_lsb = 1 << (E - 23 - 1);
+        unsigned round_to_even = (lsb_is_one && truncation >= half_lsb) ||
+                                 (!lsb_is_one && truncation > half_lsb);
+        M >>= E - 23;
         M += round_to_even;
-        // printf("2.M=0x%X\n", M);
+        if (M == 1 << 24)
+        {
+            // M >>= E - 23 后，(1<<23) <= M < (1<<24)
+            // 但，M += round_to_even 有可能导致了 M 的最高位进一
+            // 导致 M == 1 << 24 ， 例如 i = INT_MAX 就会发生这种情况
+            // 为了使得 (1<<23) <= M < (1<<24)
+            M >>= 1;
+            E++;
+        }
     }
 
-    if ((M >> 24) == 1)
-    {
-        M >>= 1;
-        exp++;
-    }
-
-    // printf("3.M=0x%X\n", M);
-
+    unsigned exp = E + 127;
     unsigned frac = M - (1 << 23);
 
-    // printf("sign = %.8X, exp = %.8X, frac = %.8X\n", sign << 31, exp << 23, frac);
     return sign << 31 | exp << 23 | frac;
 }
 
 int main(int argc, char const *argv[])
 {
 
-    // printf("INT_MIN+1 = 0x%8X\n", f2fd((float)(INT_MIN + 1)));
-
     for (int i = INT_MIN; i <= INT_MAX; i++)
     {
-        printf("float_i2f(%d) = 0x%.8X, answer is 0x%.8X\n", i, float_i2f(i), f2fd((float)i));
+        printf("float_i2f(%d) = 0x%.8X, expected = 0x%.8X\n", i, float_i2f(i), f2fd((float)i));
         assert(float_i2f(i) == f2fd((float)i));
     }
 
