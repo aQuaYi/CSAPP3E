7db29bf8222f731500eedf51d826a0a2bfe84be4
aQua <aQua@aQuaYi.com>
2018-08-08 14:50:51 +0800

    03.63 added

---
+++ homework/03/3.63.c
@@ -0,0 +1,24 @@
+long switch_prob(long x, long n)
+{
+    long result = x;
+
+    switch (n)
+    {
+    case 60:
+    case 62:
+        result = x * 8;
+        break;
+    case 63:
+        result = x / 8;
+        break;
+    case 64:
+        x *= 15;
+    case 65:
+        x *= x;
+    default:
+        result += x + 0x4B;
+        break;
+    }
+
+    return result;
+}
