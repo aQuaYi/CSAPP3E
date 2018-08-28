573e12c8d5f413f69d7a90210ab8e720e569a559
aQua <aQua@aQuaYi.com>
2018-08-28 11:57:57 +0800

    07.07 adding

---
+++ homework/07/07/README.md
@@ -0,0 +1,7 @@
+# 7.7
+
+把 bar5.c 的第 2 行修改为
+
+```C
+static double x
+```
---
+++ homework/07/07/foo5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void f(void);
+
+int y = 15212;
+int x = 15213;
+
+int main()
+{
+    f();
+    printf("x = 0x%x, y = 0x%x \n", x, y);
+    return 0;
+}
