6ca1404a3aa85e463073c8f06017359538bf28a5
aQua <aQua@aQuaYi.com>
2018-09-06 09:32:17 +0800

    格式化了代码

--- homework/08/07/snooze.c
+++ homework/08/07/snooze.c
@@ -1,29 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <csapp.h>
-
-
+#include "csapp.h"
 
 void sigint_handler(int sig)
 {
-
-    printf("CTRL+C");
-exit(0);
+    printf("\nCTRL+C\n");
 }
 
-unsigned int  snooze(unsigned int secs)
+unsigned int snooze(unsigned int secs)
 {
     unsigned int rs = sleep(secs);
-    if (signal(SIGINT, sigint_handler)== SIG_ERR) {
-        unix_error("signal error");
-    }
-    printf("Slept for %d for %d secs.", secs-rs, secs);
+    printf("Slept for %d for %d secs.\n", secs - rs, secs);
     return 0;
 }
 
 int main(int argc, char const *argv[])
 {
-  unsigned int secs = argv[1];
-snooze(secs);
+    if (signal(SIGINT, sigint_handler) == SIG_ERR)
+    {
+        printf("signal error");
+    }
+    unsigned int secs = atoi(argv[1]);
+    snooze(secs);
     return 0;
 }
