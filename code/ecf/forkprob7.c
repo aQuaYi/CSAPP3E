0b6e6c36dd5ad552956c60160f69a03984b11ebd
aQua <aQua@aQuaYi.com>
2018-09-06 10:17:17 +0800

    8.16 finish

--- code/ecf/forkprob7.c
+++ code/ecf/forkprob7.c
@@ -2,15 +2,17 @@
 #include "csapp.h"
 int counter = 1;
 
-int main() 
+int main()
 {
-    if (fork() == 0) {
-	counter--;  
-	exit(0);
+    if (fork() == 0)
+    {
+        counter--;
+        exit(0);
     }
-    else {
-	Wait(NULL); 
-	printf("counter = %d\n", ++counter);
+    else
+    {
+        wait(NULL);
+        printf("counter = %d\n", ++counter);
     }
     exit(0);
 }
